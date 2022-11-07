6ac362488caf694d0cecb1d498fa999a5b2c87cb
Adrian Immel <adrian.immel@hm.edu>
2022-11-07 23:14:43 +0100

    updated

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,43 +1,55 @@
 #include <Arduino.h>
 #include <WiFiManager.h> 
 #include <PubSubClient.h>
-#include <iostream>
-#include <PololuLedStrip.h>
+//#include <iostream>
+#include <Adafruit_NeoPixel.h>
 
 
-bool status;
 
+//configure Pins and MQTT Topics
+const int ledPin = 23;
 const int pinArray[7] = {32, 19, 21, 18, 25, 33, 22};
 const String topicArray[7] ={"Raum_Rechts", "Raum_Mitte", "Raum_Links", "Fenster_Rechts", "Fenster_Mitte", "Fenster_Links", "E_Ecke"};
+const IPAddress mqtt_server(00,00,00,00);
+#define mqttUser = "XXXXX"
+#define mqttPass = "XXXXX"
+
+
 const int ArraySize = sizeof(pinArray)/sizeof(pinArray[0]);
-const IPAddress mqtt_server(127, 0, 0, 1);
-const int ledPin = 23;
-
-
-
-
-
+bool status;
 
 //MQTT Setup
 WiFiClient espClient;
 PubSubClient mqtt(espClient);
 
 // Setup LED
-  PololuLedStrip<ledPin> ledStrip;
-  rgb_color colors[ArraySize];
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(ArraySize, ledPin, NEO_GRB + NEO_KHZ800);
+
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  String stringTopic = topic;
+      for (int i=0;i<ArraySize;i++){
+        if (String loopTopic = "zigbee2mqtt/" + topicArray[i]; stringTopic == loopTopic);
+          if (String s(payload, sizeof(payload)); s == "ON")
+          {
+            pixels.setPixelColor(pinArray[i], pixels.Color(255,255,255));
+            pixels.show();
+
+          }
+      }
+} 
 
 void setup() {  
   //init IO Pins
     for (int i=0;i<ArraySize;i++) 
       pinMode(pinArray[i], INPUT_PULLUP);
-
-
-  
+  pixels.begin();
   //init serial
-   
   Serial.begin(115200);
+
   //init wifi
   WiFiManager wifiManager;
+  
   //start wifi config wizard  
   status = wifiManager.autoConnect("fablux");
   if(!status) {
@@ -46,38 +58,24 @@ void setup() {
     } 
   else {
         //if you get here you have connected to the WiFi    
-        Serial.println("connected...)");
-        Serial.println("The Ip address is:" );
+        Serial.println("connected... :)");
         mqtt.setServer(mqtt_server, 1883);
         mqtt.setCallback(mqttCallback);
-
-
-
   }
 }
 
 void loop() {
   
   for(int i=0;i < ArraySize; i++) {
-    if (!digitalRead(pinArray[i]))
-    mqtt.publish( ("zigbee2mqtt/" + topicArray[i] + "/set/state").c_str(), "TOGGLE");
+    if (!digitalRead(pinArray[i])){
+      Serial.println("Button " + topicArray[i] + " got pressed");
+      mqtt.publish( ("zigbee2mqtt/" + topicArray[i] + "/set/state").c_str(), "TOGGLE");
+      while (!digitalRead(pinArray[i])){
+        delay(10);
+      }
+      
+    }
   }
+  delay(10);
   mqtt.loop();
 }
-
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
-  String stringTopic = topic;
-      for (int i=0;i<ArraySize;i++){
-        if (String loopTopic = "zigbee2mqtt/" + topicArray[i]; stringTopic == loopTopic);
-          if (String s(payload, sizeof(payload)); s == "ON")
-          {
-            colors[i] = rgb_color(255, 255, 255);
-          }
-          else if (String s(payload, sizeof(payload)); s == "OFF")
-          {
-            colors[i] = rgb_color(0, 0, 0);
-          }
-      }
-
-
-} 
